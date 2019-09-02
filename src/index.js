31c35239a2910e039d92fa815caf6be100616ff0
HyeYeonCha <gpdus335@naver.com>
2019-09-02 19:57:09 +0900

    마지막꺼

--- src/index.js
+++ src/index.js
@@ -1,53 +1,55 @@
 const express = require("express");
+const _ = require("lodash");
 const app = express();
-let users = [];
+let users = [{
+    id: 1,
+    name: "홍길동"
+},{
+    id: 2,
+    name: "강철수"
+}];
 let user = "null";
 
-app.use(express.json());    //  
+app.use(express.json()); 
 app.use(express.urlencoded({extended:true}));
 
-app.listen(3030);
+app.listen(3000);
+
+// app.get("/", (req, res) => {
+//     let a= 10;
+//     let b = 20;
+//     let c = a+b;
+//     req.send(c);
+//     res.send("hello dsdsad")
+// });
 
 app.get("/users", (req, res) => {
-    if(user){
-        res.send("user get" + user.name);
-    }else{
-        res.send("error")     
-    }
+  res.send("user get" + users.name);
 });
 
 app.get("/users/:id", (req,res)=> {
-    if(user && user.id == req.params.id){
-        res.send("user " + user.name+ "get");
-    }else{
-        res.send("error")     
+    if(user.id == req.params){
+        
+        res.send("user " + users.name+ "get");
     }
+    res.send("user "+ users.name + " get");
 });
 
 app.post("/users", (req,res)=> {
-    user = req.body;
-    res.send("user add"+ user.name + " post");
+    const check_user = _.find(users, ["id", req.params.id]);
+    let msg = req.body.id+" 아이디를 가진 유저가 이미 존재합니다.";
+    let success = false;
+    if(!check_user){
+        users.push(req.body);
+        msg = req.body.name+" 유저가 추가되었습니다.";
+        success = true;
+    }
+    res.send({msg, success})
 });
 app.put("/users/:id", (req,res)=> {
-    if(user && user.id == req.params.id){
-        res.send("user "+ req.params.id + " put");
-    }else{
-        res.send("error")             
-    }
+    res.send("user "+ req.params.id + " put");
 });
 app.delete("/users/:id", (req,res)=> {
-    if(user && user.id == req.params.id){
-        user = null
-        res.send("user "+ req.params.id + " delete");
-    }
-    else{
-        res.send("user id" + req.params.id+"not exist")
-    }
+    if(user)
+    res.send("user id "+ req.params.id + " 삭제");
 });
-
-
-
-
-
-
-
